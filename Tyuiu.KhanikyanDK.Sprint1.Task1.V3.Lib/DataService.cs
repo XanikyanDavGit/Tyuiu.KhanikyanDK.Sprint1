2fbddbf7fe12b309a89cd98cbd807321b1e56bd6
xanik <xanikyan00@gmail.com>
2025-09-21 21:27:13 +0500

    Task2

--- Tyuiu.KhanikyanDK.Sprint1.Task1.V3.Lib/DataService.cs
+++ Tyuiu.KhanikyanDK.Sprint1.Task1.V3.Lib/DataService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Tyuiu.KhanikyanDK.Sprint1.Task1.V3.Lib;
-using tyuiu.cources.programming.interfaces.Sprint1; //dll
+﻿using tyuiu.cources.programming.interfaces.Sprint1; //dll
 namespace Tyuiu.KhanikyanDK.Sprint1.Task1.V3.Lib
 {
     public class DataService : ISprint1Task1V3

730efe97dd36f28fc95c88281d3121c0eddbcf8f
xanik <xanikyan00@gmail.com>
2025-09-22 03:01:01 +0500

    Task2

---
+++ Tyuiu.KhanikyanDK.Sprint1.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KhanikyanDK.Sprint1.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
